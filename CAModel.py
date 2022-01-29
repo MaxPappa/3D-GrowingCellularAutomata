f57d8d75cb0d861badaf0e0eda51a8b014e0ed60
Massimiliano Pappa <maxpappa@MassimiianosMBP.home>
2022-01-29 03:40:37 +0100

    make_cube_damage and take_cube are nearly the same of those functions in VoxelDataModule with just a few changes due to the fact that here will be used only on batches with len 1.
    validation_step defined, logging loss_from_seed and loss_reconstructed. The last one
    is used to understand how the model is recovering from huge damages or from starting to
    regions unexplored by the starting seed in the first steps.
    
    training_step modifications:
    now the model is used two times on two inputs. The first one is the seed, the second
    is the previously computed output (first input) which will be damaged with the take_cube function.
    Those two outputs will then be used to calculate a loss which is the average of the seed_loss and
    reconstruction_loss. In this way, the model is FORCED to understand how to recover from every type and
    amount of damage.
    
    in the __init__ method (constructor) weight_norm is used, and kernel_x/y/z are now used as attribute of the
    model thanks to the self.register_buffer method (this will avoid unnecessary computation of kernels at each update
    step of each train step of each epoch.. which is quite a lot of useless computation).

--- CAModel.py
+++ CAModel.py
@@ -1,13 +1,14 @@
 import torch
 import torch.nn as nn
+from torch.nn.utils import weight_norm
 import torch.nn.functional as F
 import pytorch_lightning as pl
-from visualizationUtils import visualizeGO
+from visualizationUtils import visualizeGO, visualizeImprovements
 from typing import Union, Dict, List
 from Config import ModelConfig
 from dataclasses import asdict
 from MyCallbacks import PoolSamplerCallback
-from random import randint
+from random import randint, sample
 
 class CAModel(pl.LightningModule):
     def __init__(self, hparams: Union[Dict, ModelConfig], min_step:int = 50, max_step: int = 95):
@@ -17,11 +18,21 @@ class CAModel(pl.LightningModule):
         self.min_step = min_step
         self.max_step = max_step
 
-        self.fc0 = nn.Linear(self.hparams["n_channels"]*4, self.hparams["hidden_size"])
+        self.fc0 = weight_norm(nn.Linear(self.hparams["n_channels"]*4, self.hparams["hidden_size"]))
         self.fc1 = nn.Linear(self.hparams["hidden_size"], self.hparams["n_channels"], bias=False)
         with torch.no_grad():
             self.fc1.weight.zero_()
         self.automatic_optimization = True
+
+        kernel = torch.tensor([[[1, 2, 1], [0, 0, 0], [-1, -2, -1]],
+                                      [[2, 4, 2], [0, 0, 0], [-2, -4, -2]],
+                                      [[1, 2, 1], [0, 0, 0], [-1, -2, -1]]]) / 26
+        self.register_buffer("kernel_x", kernel[None,None,...].repeat(self.hparams.n_channels,1,1,1,1))
+
+        self.register_buffer("kernel_y", kernel.transpose(0,1)[None,None,...].repeat(self.hparams.n_channels,1,1,1,1))
+
+        self.register_buffer("kernel_z", kernel.transpose(1,2)[None,None,...].repeat(self.hparams.n_channels,1,1,1,1))
+
 
     def alive(self, x):
         return F.max_pool3d(x[:, 3:4, :, :, :], kernel_size=3, stride=1, padding=1) > 0.1
@@ -29,18 +40,12 @@ class CAModel(pl.LightningModule):
     def perceive(self, x, angle):
 
         def _perceive_with(x, kernel):
-            kernel = kernel[None,None,...].repeat(self.hparams.n_channels, 1, 1, 1, 1)
             return F.conv3d(x, kernel, padding=1, groups=self.hparams.n_channels)
 
-        kernel_x = torch.tensor([[[1, 2, 1], [0, 0, 0], [-1, -2, -1]],
-                             [[2, 4, 2], [0, 0, 0], [-2, -4, -2]],
-                             [[1, 2, 1], [0, 0, 0], [-1, -2, -1]]], device=self.hparams.device) / 26
-        kernel_y = kernel_x.transpose(0,1)
-        kernel_z = kernel_x.transpose(1,2)
+        y1 = _perceive_with(x,self.kernel_x)
+        y2 = _perceive_with(x,self.kernel_y)
+        y3 = _perceive_with(x,self.kernel_z)
 
-        y1 = _perceive_with(x,kernel_x)
-        y2 = _perceive_with(x,kernel_y)
-        y3 = _perceive_with(x,kernel_z)
         y = torch.cat((x,y1,y2,y3),1)
         return y
 
@@ -74,15 +79,74 @@ class CAModel(pl.LightningModule):
             if path:
                 fig = visualizeGO(x[0], True)
                 fig.write_image(f"{path}_{step}.png")
-        return x
+        return x.detach()
 
     def training_step(self, train_batch:torch.tensor, batch_idx:int) -> float:
-        x, target = train_batch[0]
-        for step in range(0, randint(self.min_step, self.max_step+1)):
+        x, target = train_batch
+        num_step = randint(self.min_step, self.max_step)
+        for step in range(0, num_step):
             x = self.update(x, self.hparams['fire_rate'])
-        loss = F.mse_loss(x[:, :, :, :, :4], target[:,:,:,:,:4])
+        loss_growth = F.mse_loss(x[:, :, :, :, :4], target[:,:,:,:,:4])
+        x_2 = self.take_cube(x.detach().clone())
+        for step in range(0, num_step-20):
+            x_2 = self.update(x_2, self.hparams['fire_rate'])
+        loss_reconstructed = F.mse_loss(x_2[:,:,:,:,:4], target[:,:,:,:,:4])
+        loss = (loss_growth + loss_reconstructed)/2
+        self.log("train_lossgrowth", loss_growth)
+        self.log("train_lossreconstructed", loss_reconstructed)
         self.log("train_loss", loss)
         return {"loss":loss, "out":x.detach()}
+
+    def make_cube_damage(self, inp):
+        lungh = len(torch.where(inp[:,:,:,:,3:4]>0.1)[0])
+        if lungh <=1:
+            return inp
+        num = sample(range(0,lungh-1), k=1)[0]
+        coords = torch.where(inp[:,:,:,:,3:4]>0.1)
+        x,y,z = coords[1][num],coords[2][num],coords[3][num]
+        r = randint(4,8)
+        inp[:,max(0,x-r):x+r, max(0,y-r):y+r, max(0,z-r):z+r,:] = 0
+        return inp
+
+    def take_cube(self, inp):
+        side = randint(5,8)
+        x,y,z = torch.where(inp[:,:,:,:,3:4] > 0.1)[1:4]
+        if len(x) == 0:
+            return inp
+        # pdb.set_trace()
+        index = randint(0,len(x)-1)
+        x,y,z = x[index],y[index],z[index]
+        block = inp[:, max(0,x - side) : x + side, max(0,y - side) : y + side, max(0, z - side) : z + side, :].clone()
+
+        # this is to avoid collapse of patterns in pool (it can happen that sometimes it can't reconstruct nothing and in the pool we
+        # will have something that collapse with zero mature/growing cells, so with an empty/ghost object)
+        if block[:,:,:,:,3:4].sum() <= 10: 
+            return inp 
+
+        inp[:, :, :, :, :] = 0
+        inp[:, max(0,x - side) : x + side, max(0,y - side) : y + side, max(0, z - side) : z + side, :] = block.clone()
+        return inp
+
+    def validation_step(self, batch, batch_idx):
+        x, target = batch
+        x = self(x, steps=randint(self.min_step, self.max_step+1), fire_rate=self.hparams.fire_rate)
+        #pdb.set_trace()
+        damaged = self.take_cube(x.detach().clone()) #self.make_cube_damage(x.detach().clone())
+        reconstructed = damaged.clone()
+        reconstructed = self(reconstructed, steps=randint(self.min_step, self.max_step+1), fire_rate=self.hparams.fire_rate)
+        loss = F.mse_loss(x[:,:,:,:,:4], target[:,:,:,:,:4])
+        loss_reconstruction = F.mse_loss(reconstructed[:,:,:,:,:4], x[:,:,:,:,:4])
+        self.log("loss_from_seed", loss)
+        self.log("loss_reconstructed", loss_reconstruction)
+        if self.trainer.current_epoch % 2 == 0:
+            x = x.squeeze()
+            damaged = damaged.squeeze()
+            reconstructed = reconstructed.squeeze()
+            self.trainer.logger.experiment.log({"DAMAGED - before and after" : visualizeImprovements(x, damaged)})
+            self.trainer.logger.experiment.log({"DAMAGED - Growing improvements " : visualizeImprovements(reconstructed, damaged)})
+        return {"loss_from_seed":loss, "out":x, "damaged":damaged, "reconstructed":reconstructed, "loss_reconstructed":loss_reconstruction}
+
+
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams["lr"], betas=self.hparams["betas"])
