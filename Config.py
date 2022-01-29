e14b39542313e1450e2faeadec5636a25cb6cc4e
Massimiliano Pappa <maxpappa@MassimiianosMBP.home>
2022-01-29 03:08:14 +0100

    changed a few things in Config.py and requirements.txt

--- Config.py
+++ Config.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from typing import Tuple
 
-
 @dataclass
 class ModelConfig:
     n_epochs: int = 300  # number of epochs of training
@@ -13,15 +12,13 @@ class ModelConfig:
     fire_rate: float = 0.5
     n_channels: int = 16  # number of image channels
     hidden_size: int = 128
-    # dataloader parameters
-    n_cpu: int = 8  # number of cpu threads to use for the dataloaders
-    device: str = "cuda"
 
 @dataclass
 class DataModuleConfig:
-    filePath: str = "./plyFiles/Wildebeest.ply"
+    filePath: str = "./plyFiles/large puffin.ply"
     channels: int = 16
     pool_size: int = 128
     batch_size: int = 4
     padding: int = 0
-    num_workers: int = 2
+    num_workers: int = 0
+    damage: bool = 1
