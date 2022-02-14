6544b92080fbf41c704846ef18ca2114cb52aa4a
Massimiliano Pappa <maxpappa@MassimiianosMBP.home>
2022-02-14 19:08:15 +0100

    A few modifications to the on_before_batch_transfer method, in order to include cropping damage and noisy pixels during training.

--- VoxelDataModule.py
+++ VoxelDataModule.py
@@ -17,26 +17,29 @@ class VoxelDataModule(pl.LightningDataModule):
 
     def prepare_data(self, *args, **kwargs):
         self.target = readPLY(self.hparams["filePath"])
+        self.batch_idx = list(range(0,self.hparams["batch_size"]))
         listCoords = []
+
         coords = np.where(self.target[:,:,:,3:4]==1)
         for i in range(0,len(coords[0])):
-            listCoords += [(coords[0][i:i+1][0], coords[1][i:i+1][0], coords[2][i:i+1][0])]
+            listCoords += [np.array([coords[0][i:i+1][0], coords[1][i:i+1][0], coords[2][i:i+1][0]])]
 
-        centerCoords = getCentroid(listCoords)
+        centerCoords = getCentroid(listCoords, np.array(self.target.shape[:3])//2)
         self.listCoords = listCoords
-
         self.seed = np.zeros(list(self.target.shape)[:-1]+[self.hparams["channels"]], np.float32)
         self.seed[centerCoords[0],centerCoords[1],centerCoords[2], 3:] = 1.0
+
         self.pool = torch.from_numpy(np.repeat(self.seed[None, ...], self.hparams["pool_size"], 0))
         self.pool_target = torch.from_numpy(np.repeat(self.target[None, ...], self.hparams["pool_size"], 0))
         self.seed = torch.from_numpy(self.seed)
         self.target = torch.from_numpy(self.target)
 
     def setup(self, stage):
-        self.train_data = TensorDataset(self.pool, self.pool_target)# torch.from_numpy(self.target))
+        self.train_data = TensorDataset(self.pool, self.pool_target)
 
     def train_dataloader(self):
-        self.train_data = TensorDataset(self.pool, self.pool_target)# reload at each epoch setting Trainer flag "reload_dataloaders_every_n_epocs=1"
+        # reload at each epoch setting Trainer flag "reload_dataloaders_every_n_epocs=1"
+        self.train_data = TensorDataset(self.pool, self.pool_target)
         dataloader =  DataLoader(self.train_data, batch_size=self.hparams.batch_size, num_workers=self.hparams.num_workers)
         return dataloader
 
@@ -68,33 +71,56 @@ class VoxelDataModule(pl.LightningDataModule):
         return batch
 
     def take_cube(self, inp):
-        for i in range(0,inp.shape[0]):
-            r = randint(5,8)
-            x,y,z = torch.where(inp[i,:,:,:,3:4] > 0.1)[1:4]
-            if len(x) == 0:
-                continue
-            index = randint(0,len(x)-1)
-            x,y,z = x[index],y[index],z[index]
-            block = inp[i, max(0,x - r) : x + r, max(0,y - r) : y + r, max(0, z - r) : z + r, :].clone()
-
-            # this is to avoid collapse of patterns in pool (it can happen that sometimes it can't reconstruct nothing and in the pool we
-            # will have something that collapse with zero mature/growing cells, so with an empty/ghost object)
-            if block[:,:,:,3:4].sum() <= 10: 
-                 continue
-            
-            inp[i,:, :, :, :] = 0
-            inp[i, max(0,x - r) : x + r, max(0,y - r) : y + r, max(0, z - r) : z + r, :] = block.clone()
+        x_s, y_s, z_s = inp.shape[1:4]
+        min_side_x, min_side_y, min_side_z = max(3,x_s//5), max(3,y_s//5), max(3,z_s//5)
+        max_side_x, max_side_y, max_side_z = x_s//3, y_s//3, z_s//3
+        if min_side_x > max_side_x or min_side_y > max_side_y or min_side_z > max_side_z:
+            return inp
+        side_x = randint(min_side_x,max_side_x)
+        side_y = randint(min_side_y,max_side_y)
+        side_z = randint(min_side_z,max_side_z)
+        x,y,z = torch.where(inp[:, side_x:x_s-side_x, side_y:y_s-side_y, side_z:z_s-side_z, 3:4] > 0.1)[1:4]
+        if len(x) == 0:
+            return inp
+        index = randint(0,len(x)-1)
+        x,y,z = x[index],y[index],z[index]
+        block = inp[:, max(0,x - side_x) : x + side_x, max(0,y - side_y) : y + side_y, max(0, z - side_z) : z + side_z, :].clone()
+        idx = torch.where((block[:,:,:,:,3:4]>0.1).sum(dim=[1,2,3,4]) >= 64)[0]
+        if len(idx) == 0:
+            return inp
+        inp[idx, :, :, :, :] = 0
+        inp[idx, max(0,x - side_x) : x + side_x, max(0,y - side_y) : y + side_y, max(0, z - side_z) : z + side_z, :] = block[idx].clone()
         return inp
 
+    def percentageAdversarialCells(inp, perc):
+        indices = (inp[:, :, :, :, 3:4] > 0.1)[0,:, :, :, 0].squeeze().nonzero(as_tuple=False)
+        mask = torch.rand(indices.shape[0]) <= perc
+        indices = indices[mask,:]
+        noise = torch.rand(list(inp.shape[1:4])+[16], dtype=inp.dtype)
+        inp[:, indices[:,0], indices[:,1], indices[:,2],:] = noise[indices[:,0],indices[:,1],indices[:,2],:]
+        return inp
+
+    def perturbate(self, inp, perc):
+        for i in range(0, inp.shape[0]):
+            indices = (inp[i,:,:,:,3:4]>0.1)[:,:,:,0].squeeze().nonzero(as_tuple=False)
+            mask = torch.rand(indices.shape[0]) <= perc
+            indices = indices[mask,:]
+            noise = torch.rand(list(inp.shape[1:4])+[16], dtype=inp.dtype)
+            inp[i, indices[:,0], indices[:,1], indices[:,2],:] = noise[indices[:,0],indices[:,1],indices[:,2],:]
+        return inp
 
     def on_before_batch_transfer(self, batch: torch.tensor, dataloader_idx: int) -> torch.tensor:
-        # calculate loss between initial batch and target, sort their indexes (in the batch) in descending order
-        # using calculated loss, then take the first one (which has the highest loss in the batch)
+        # calculate loss between initial batch and target, sort their indexes (in the batch) in ascending order
+        # using calculated loss, then take the last one (which has the lower loss in the batch)
         if self.trainer.validating:
             return batch
         with torch.no_grad():
-            idx_worst = torch.mean(torch.pow(batch[0][0][..., :4] - self.target, 2), [-1,-2,-3,-4]).argsort(descending=True)[:1]
-            batch[0][idx_worst[0]] = self.seed.clone()
-            if self.hparams.damage and self.trainer.global_step > 100:
-                batch[0][1:] = self.take_cube(batch[0][1:])
+            idx_worst = torch.mean(torch.pow(batch[0][..., :4] - self.target, 2), [-1,-2,-3,-4]).argsort(descending=True)[:3]
+            batch[0][idx_worst[:1]] = self.seed.clone()
+            if self.hparams.modifiedTrainingMode:
+                batch[0][idx_worst[1:]] = self.take_cube(batch[0][idx_worst[1:]])
+                if self.hparams.randomNoise:
+                    batch[0][idx_worst[-1]] = self.perturbate(batch[0][idx_worst[-1]][None,...], perc=0.05)
+            else:
+                batch[0][idx_worst[1:]] = self.make_cube_damage(batch[0][idx_worst[1:]])
         return batch
