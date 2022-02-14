dedaede4b28fc8192fd75bdff31634a60eeab1c1
Massimiliano Pappa <maxpappa@MassimiianosMBP.home>
2022-02-14 19:06:32 +0100

    added a few flags to understand which kind of training mode we're using

--- Config.py
+++ Config.py
@@ -3,9 +3,9 @@ from typing import Tuple
 
 @dataclass
 class ModelConfig:
-    n_epochs: int = 300  # number of epochs of training
+    n_epochs: int = 400  # number of epochs of training
     # adam parameters
-    lr: float = 2e-3  # adam: learning rate
+    lr: float = 3e-3  # adam: learning rate
     lr_gamma: float = 0.9999
     betas: Tuple[float] = (0.5,0.5)
     # CAModel parameters
@@ -19,6 +19,7 @@ class DataModuleConfig:
     channels: int = 16
     pool_size: int = 128
     batch_size: int = 4
-    padding: int = 0
     num_workers: int = 0
-    damage: bool = 1
+    damage: bool = True
+    modifiedTrainingMode: bool = True # True for the classic version of training
+    randomNoise: bool = True
